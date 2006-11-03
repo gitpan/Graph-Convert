85652dc2c782339f7f05c54c1bf4f1fdb89ad13f
Tels <nospam-abuse@bloodgate.com>
2006-11-03 15:41:12 +0000

    Import of TELS/Graph-Convert-0.04 from CPAN.
    gitpan-cpan-distribution: Graph-Convert
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         TELS/graph/Graph-Convert-0.04.tar.gz
    gitpan-cpan-author:       TELS
    gitpan-cpan-maturity:     released

--- lib/Graph/Convert.pm
+++ lib/Graph/Convert.pm
@@ -10,23 +10,19 @@ use 5.008001;
 use Graph::Easy;
 use Graph;
 
-$VERSION = '0.03';
+$VERSION = '0.04';
 
 use strict;
 
 #############################################################################
 # conversion
 
-sub as_graph
+sub _add_basics
   {
-  # convert a Graph::Easy object to a Graph object
-  my ($self,$in) = @_;
-
-  Graph::Easy::Base->_croak(
-    "as_graph needs a Graph::Easy object, but got '". ref($in). "'" )
-   unless ref($in) && $in->isa('Graph::Easy');
- 
-  my $out = Graph->new( multiedged => 1); 
+  # Add the graph and class attributes from $in to $out
+  # Add all the nodes
+  # Add the groups as pseudo_attributes to the graph (so we can recover them)
+  my ($self, $in, $out) = @_;
 
   # add the graph attributes
   my $att = $in->{att};
@@ -49,6 +45,48 @@ sub as_graph
     $out->set_vertex_attributes($n->{name}, $attr);
     }
 
+  $out;
+  }
+
+sub as_graph
+  {
+  # convert a Graph::Easy object to a Graph object
+  my ($self,$in) = @_;
+
+  $self->error(
+    "as_graph needs a Graph::Easy object, but got '". ref($in). "'" )
+   unless ref($in) && $in->isa('Graph::Easy');
+  
+  return $self->as_multiedged_graph($in) unless $in->is_simple_graph();
+
+  my $out = Graph->new(); 
+
+  $self->_add_basics($in,$out);
+
+  # add all edges
+  for my $e ($in->edges())
+    {
+    my $from = $e->{from}->{name}; my $to = $e->{to}->{name};
+    my $edge = $out->add_edge($from,$to);
+    my $attr = $e->raw_attributes();
+    $out->set_edge_attributes($from, $to, $attr);
+    }
+
+  $out;
+  }
+
+sub as_multiedged_graph
+  {
+  my ($self,$in) = @_;
+
+  $self->error(
+    "as_multiedged_graph needs a Graph::Easy object, but got '". ref($in). "'" )
+   unless ref($in) && $in->isa('Graph::Easy');
+ 
+  my $out = Graph->new(multiedged => 1); 
+
+  $self->_add_basics($in,$out);
+
   # add all edges
   for my $e ($in->edges())
     {
@@ -67,7 +105,7 @@ sub as_graph_easy
   # convert a Graph object to a Graph::Easy object
   my ($self,$in) = @_;
  
-  Graph::Easy::Base->_croak(
+  $self->error(
     "as_graph_easy needs a Graph object, but got '". ref($in). "'" )
    unless ref($in) && $in->isa('Graph');
  
@@ -91,14 +129,29 @@ sub as_graph_easy
     my $attr = $in->get_vertex_attributes($n);
     $node->set_attributes($attr);
     }
-  for my $e ($in->edges())
+
+  if ($in->is_multiedged())
     {
-    # get all the IDs in case of the edge existing more than once:
-    my @ids = $in->get_multiedge_ids($e->[0], $e->[1]);
-    for my $id (@ids)
+    # for multiedged graphs:
+    for my $e ($in->unique_edges())
+      {
+      # get all the IDs in case of the edge existing more than once:
+      my @ids = $in->get_multiedge_ids($e->[0], $e->[1]);
+      for my $id (@ids)
+        {
+        my $edge = $out->add_edge($e->[0],$e->[1]);
+        my $attr = $in->get_edge_attributes_by_id($e->[0], $e->[1], $id);
+        $edge->set_attributes($attr);
+        }
+      }
+    }
+  else
+    {
+    # for simple graphs
+    for my $e ($in->edges())
       {
       my $edge = $out->add_edge($e->[0],$e->[1]);
-      my $attr = $in->get_edge_attributes_by_id($e->[0], $e->[1], $id);
+      my $attr = $in->get_edge_attributes($e->[0], $e->[1]);
       $edge->set_attributes($attr);
       }
     }
@@ -119,6 +172,7 @@ Graph::Convert - Convert between graph formats: Graph and Graph::Easy
 	
 	my $graph_easy = Graph::Easy->new();
 	$graph_easy->add_edge ('Bonn', 'Berlin');
+	$graph_easy->add_edge ('Berlin', 'Berlin');
 
 	# from "Graph::Easy" to "Graph"
 	my $graph = Graph::Convert->as_graph ( $graph_easy );
@@ -128,27 +182,70 @@ Graph::Convert - Convert between graph formats: Graph and Graph::Easy
 
 	print $ge->as_ascii( );
 
-	# prints:
+	# Outputs something like:
 
+	#                +----+
+	#                v    |
 	# +------+     +--------+
 	# | Bonn | --> | Berlin |
 	# +------+     +--------+
 
 =head1 DESCRIPTION
 
-C<Graph::Convert> lets you convert graphs between some popular graph formats:
+C<Graph::Convert> lets you convert graphs between the graph formats
+from L<Graph> and L<Graph::Easy>.
 
-	Graph
-	Graph::Easy
+It takes a graph object in either format, and converts it to the desired
+output format. It handles simple graphs as well as multi-edged graphs,
+and also carries the attributes over.
+
+This enables you to use all the layout and formatting capabilities
+of C<Graph::Easy> on C<Graph> objects, as well as using the extensive
+graph algorithms and manipulations of C<Graph> on C<Graph::Easy> objects.
 
 X<graph>
-X<graph::easy>
+X<easy>
+X<graph-easy>
 X<conversion>
 X<convert>
 
-=head2 Input
+=head2 Graph vs. Graph::Easy
 
-=head2 Output
+Both C<Graph> and C<Graph::Easy> represent graphs, e.g. vertices (or nodes)
+connected by edges. These graphs can have (arbitrary) attributes attached
+to the graph, nodes or edges.
+
+Both formats can serialize the graph by creating a text-representation,
+but unlike C<Graph::Easy>, C<Graph> is not able to create the graph back
+from the string form.
+
+There are, however, some slight differences between these two packages:
+
+=over 12
+
+=item Graph
+
+C<Graph> has different representations for multi-edges and simple graphs,
+making it somewhat complicated to switch between these two.
+
+It does have an extensive set of algorithms to manipulate the graph, but no
+layout capabilities.
+
+=item Graph::Easy
+
+C<Graph::Easy> uses the same representation for multi-edged and simple graphs,
+but has only basic operations to manipulate the graph.
+
+It has, however, a build-in layouter which can lay out the graph on a
+grid, as well the ability to output graphviz code. This enables output
+of ASCII, HTML, SVG and all the formats that graphviz supports, like PNG.
+
+In addition, C<Graph::Easy> supports class attributes. By setting the
+attribute for a class and putting objects (nodes/edges etc) into
+the proper class, it is easier to manipulate attributes for many
+objects at once.
+
+=back
 
 =head1 METHODS
 
@@ -163,6 +260,27 @@ C<Graph::Convert> supports the following methods:
 
 Converts the given L<Graph::Easy> object into a L<Graph> object.
 
+This routine creates either a simple or a multiedged graph, depending
+on whether the input L<Graph::Easy> object is a simple graph or not.
+
+If you want to force the output to be a multiedged graph object, use
+L<as_multiedged_graph>.
+
+Forcing the output to be a simple graph when the input is multi-edged
+is not supported, as that would require to drop arbitrary edges from
+the input.
+
+=head2 as_multiedged_graph()
+
+        use Graph::Convert;
+
+        my $graph_easy = Graph::Easy->new( );
+        my $graph = Graph::Convert->as_multiedged_graph( $graph_easy );
+
+Converts the given L<Graph::Easy> object into a multi-edged L<Graph>
+object, even if the input graph is a simple graph (meaning there
+is only one edge going from node A to node B).
+
 =head2 as_graph_easy()
 
         use Graph::Convert;
@@ -172,21 +290,10 @@ Converts the given L<Graph::Easy> object into a L<Graph> object.
 
 Converts the given L<Graph> object into a L<Graph::Easy> object.
 
-=head1 CAVEATS
+This routine handles simple as well as multiedges graphs.
 
-=over 12
-
-=item as_graph()
-
-This routine always creates a multiedged graph, even if the input
-Graph:Easy is a simple one - e.g. with never more than one edge
-leading from node A to B.
-
-=item as_graph_easy()
-
-This routine expects multiedged graph.
-
-=back
+Multi-vertexed graphs are not supported e.g. each node must exist only once
+in the input graph.
 
 =head1 SEE ALSO
 
